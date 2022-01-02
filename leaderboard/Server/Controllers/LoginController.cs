3d15f9a7dd8646c1c7c28532ebce27391fc77a9a
Kim Nilsen Brusevold <kimbrusevold@gmail.com>
2022-01-02 21:14:50 +0100

    Add JWT token generation

---
+++ leaderboard/Server/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace leaderboard.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration Configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Login([FromBody] LoginData data)
+        {
+
+
+
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "asdasd"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var secretKey = Configuration["JWT:key"];
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var token = new JwtSecurityToken(
+                issuer: Configuration["JWT:iss"],
+                audience: Configuration["JWT:aud"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return Ok(new {token = new JwtSecurityTokenHandler().WriteToken(token)});
+        }
+
+        public readonly struct LoginData
+        {
+            public string Code { get; init; }
+
+            public LoginData(string code)
+            {
+                Code = code;
+            }
+        }
+    }
+}
