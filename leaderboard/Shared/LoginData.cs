8a10f5b65050278bba3251c4b949542072cf6ea3
Kim Nilsen Brusevold <kimbrusevold@gmail.com>
2022-01-04 22:45:50 +0100

    Move these structs to shared. Will be used/consumed by client as well as server

---
+++ leaderboard/Shared/LoginData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leaderboard.Shared;
+public readonly record struct LoginData
+{
+    public string Code { get; init; }
+
+    public LoginData(string code)
+    {
+        Code = code;
+    }
+}
+
---
+++ leaderboard/Shared/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leaderboard.Shared;
+
+public readonly record struct LoginResponse
+{
+    public string Token { get; init; }
+    public DateTime Expires { get; init; }
+}
+
