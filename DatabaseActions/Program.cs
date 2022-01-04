c3f536308b8ba5a029d2013a484b6ba0d632979e
Kim Nilsen Brusevold <kimbrusevold@gmail.com>
2022-01-04 22:50:13 +0100

    Add test project to test mongo db conenction

---
+++ DatabaseActions/Program.cs
@@ -0,0 +1,57 @@
+﻿using DatabaseActions;
+using MongoDB.Driver;
+
+var settings = MongoClientSettings.FromConnectionString("");
+var client = new MongoClient(settings);
+
+
+
+
+var database = client.GetDatabase("leaderboard");
+
+var collection = database.GetCollection<User>("users");
+
+User user = new()
+{
+    UserName = "Kim",
+};
+
+//await collection.InsertOneAsync(user);
+
+
+var trackCol = database.GetCollection<Track>("tracks");
+
+var track = new Track()
+{
+    Name = "Circuit de Barcelona-Catalunya"
+};
+
+//await trackCol.InsertOneAsync(track);
+
+var barcaFilter = Builders<Track>.Filter.Eq("Name", "Circuit de Barcelona-Catalunya");
+var barca = await trackCol.Find(barcaFilter).FirstAsync();
+
+
+var timeCol = database.GetCollection<TrackTime>("track_times");
+
+var trackT = new TrackTime()
+{
+    Created = DateTime.UtcNow,
+    Time = new TimeSpan(0, 0, 2, 24, 100),
+    Track = barca,
+    User = user,
+};
+
+//await timeCol.InsertOneAsync(trackT);
+
+
+var filter = Builders<TrackTime>.Filter.Where(t => t.Track.Id == barca.Id && t.User.Id == null);
+
+
+
+var res = await timeCol.Find(filter).SortBy(t => t.Time).ToListAsync();
+
+foreach (var item in res)
+{
+    Console.WriteLine(item.Time);
+}
---
+++ DatabaseActions/User.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseActions
+{
+    public class User
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+        public string DiscordId { get; set; }
+        public string UserName { get; set; }
+        [BsonDefaultValue(false)]
+        public bool IsAdmin { get; set; } 
+    }
+
+    public class TrackTime
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+        public User User { get; set; }
+        public DateTime Created { get; set; }
+        public TimeSpan Time { get; set; }
+        public Track Track { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Game Game { get; set; }
+    }
+    public class Track
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Vehicle
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Game
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
