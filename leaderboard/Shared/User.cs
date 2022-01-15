57d22ee59ab720535aafc290396900c7da20408c
Kim Nilsen Brusevold <kimbrusevold@gmail.com>
2022-01-15 15:06:01 +0100

    Add class to represent user

---
+++ leaderboard/Shared/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leaderboard.Shared
+{
+    public class User
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
